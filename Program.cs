dd6dd542acf052ffa0622b82a70a2a612e6ce820
Teerasej <teerasej@gmail.com>
2020-09-08 15:25:55 +0700

    Use MS.Identity to build client to get token

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 
 namespace GraphAPI_1
 {
     class Program
     {
         
-        private static readonly string clientId = "38478b47-0715-4ce9-8d56-90ff0d503ce4";
-        static void Main(string[] args)
+        private static readonly string _clientId = "38478b47-0715-4ce9-8d56-90ff0d503ce4";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = PublicClientApplicationBuilder.Create(_clientId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            string[] scopes = new string[] { "User.Read" };
+
+            var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            var token = result.AccessToken;
         }
     }
 }
