274c3dffff20b94f564b663e62ac61cb173cd8b6
Teerasej <teerasej@gmail.com>
2020-09-08 17:31:50 +0700

    Show device code

--- AuthProvider.cs
+++ AuthProvider.cs
@@ -40,6 +40,7 @@ namespace GraphAPI_1
                     // Invoke device code flow so user can sign-in with a browser
                     var result = await _msalClient.AcquireTokenWithDeviceCode(_scopes, callback => {
                         Console.WriteLine(callback.Message);
+                        Console.WriteLine(callback.DeviceCode);
                         return Task.FromResult(0);
                     }).ExecuteAsync();
 
