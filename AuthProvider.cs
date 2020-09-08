3b81383e5221dd3e5d8edb0092af4a253f312968
Teerasej <teerasej@gmail.com>
2020-09-08 17:10:57 +0700

    Create auth provider to use with MS Graph API

---
+++ AuthProvider.cs
@@ -0,0 +1,69 @@
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace GraphAPI_1
+{
+    public class AuthProvider : IAuthenticationProvider
+    {
+        private IPublicClientApplication _msalClient;
+        private string[] _scopes;
+        private IAccount _userAccount;
+
+        public AuthProvider(string appId, string[] scopes)
+        {
+            _scopes = scopes;
+
+            _msalClient = PublicClientApplicationBuilder
+                .Create(appId)
+                .WithAuthority(AadAuthorityAudience.AzureAdAndPersonalMicrosoftAccount, true)
+                .Build();
+        }
+
+        public async Task AuthenticateRequestAsync(HttpRequestMessage requestMessage)
+        {
+            requestMessage.Headers.Authorization =
+                new AuthenticationHeaderValue("bearer", await GetAccessToken());
+        }
+        
+         public async Task<string> GetAccessToken()
+        {
+            // If there is no saved user account, the user must sign-in
+            if (_userAccount == null)
+            {
+                try
+                {
+                    // Invoke device code flow so user can sign-in with a browser
+                    var result = await _msalClient.AcquireTokenWithDeviceCode(_scopes, callback => {
+                        Console.WriteLine(callback.Message);
+                        return Task.FromResult(0);
+                    }).ExecuteAsync();
+
+                    _userAccount = result.Account;
+                    return result.AccessToken;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Error getting access token: {exception.Message}");
+                    return null;
+                }
+            }
+            else
+            {
+                // If there is an account, call AcquireTokenSilent
+                // By doing this, MSAL will refresh the token automatically if
+                // it is expired. Otherwise it returns the cached token.
+
+                    var result = await _msalClient
+                        .AcquireTokenSilent(_scopes, _userAccount)
+                        .ExecuteAsync();
+
+                    return result.AccessToken;
+            }
+        }
+    }
+}
