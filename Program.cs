8b2686619c7cad7c24a2c9d8b6862174094fd85e
Teerasej <teerasej@gmail.com>
2020-09-08 15:21:08 +0700

    add azure AD app's client id

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace GraphAPI_1
 {
     class Program
     {
+        
         private static readonly string clientId = "38478b47-0715-4ce9-8d56-90ff0d503ce4";
         static void Main(string[] args)
         {
