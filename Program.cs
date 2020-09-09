1385f410e9e706c139a8a0ff6822f3c6ab344bae
Teerasej <teerasej@gmail.com>
2020-09-09 22:26:46 +0700

    clear for start

--- Program.cs
+++ Program.cs
@@ -8,39 +8,13 @@ namespace GraphAPI_1
     class Program
     {
         
-        private static readonly string _clientId = "38478b47-0715-4ce9-8d56-90ff0d503ce4";
+        private static readonly string _clientId = "";
         static async Task Main(string[] args)
         {
 
             string[] scopes = new string[] { 
-                "User.Read", 
-                "Directory.Read.All"
+                "User.Read",
             };
-
-            var authProvider = new AuthProvider(_clientId, scopes);
-
-            var token = authProvider.GetAccessToken().Result;
-            Console.WriteLine($"Access token: {token}");
-
-            var photoJson = await "https://graph.microsoft.com/v1.0/me/photo/"
-                .WithOAuthBearerToken(token)
-                .GetStringAsync();
-            Console.WriteLine("My Photo Infomation:");
-            Console.WriteLine(photoJson);
-
-             var drive = await "https://graph.microsoft.com/v1.0/me/drive/"
-                .WithOAuthBearerToken(token)
-                .GetStringAsync();
-
-            Console.WriteLine("My OneDrive:");
-            Console.WriteLine(drive);
-
-
-            var users = await "https://graph.microsoft.com/v1.0/users/"
-                .WithOAuthBearerToken(token)
-                .GetStringAsync();
-            Console.WriteLine("Users in org:");
-            Console.WriteLine(users);
 
         }
     }
